6b42a7478f80a4d22784ad319417261f90fc7c0e
xiaoqiang <3160101819@zju.edu.cn>
2019-05-30 09:42:00 +0800

    HistoryRecord

---
+++ src/component/HistoryRecord.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {Table, Divider, Tag } from 'antd';
+const { Column, ColumnGroup } = Table;
+
+const data = [
+  {
+    key: '1',
+    firstName: 'John',
+    lastName: 'Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    firstName: 'Jim',
+    lastName: 'Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    firstName: 'Joe',
+    lastName: 'Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+export default class HistoryRecord extends React.Component {
+    
+}
